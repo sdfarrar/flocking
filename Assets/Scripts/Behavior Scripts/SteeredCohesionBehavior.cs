95d592e4d04ec936a6b44ad35fc7a3c5a073cd60
vague315 <vague315@gmail.com>
2019-09-09 22:42:43 -0500

    Adds cohesion with smoothing. Adds boundary behavior

---
+++ Assets/Scripts/Behavior Scripts/SteeredCohesionBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behaviors/Steered Cohesion")]
+public class SteeredCohesionBehavior : FlockBehavior {
+
+    [SerializeField] private float agentSmoothTime = 0.5f;
+
+    Vector2 currentVelocity;
+
+    public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock) {
+        // If no neighbors, then no adjustment
+        if (context.Count == 0) { return Vector2.zero; }
+
+        // Otherwise average out position of neighbors
+        Vector2 move = Vector2.zero;
+        foreach (Transform item in context) {
+            move += (Vector2)item.position;
+        }
+        move /= context.Count;
+
+        // Create offset from agent position
+        move -= (Vector2)agent.transform.position;
+        move = Vector2.SmoothDamp(agent.transform.up, move, ref currentVelocity, agentSmoothTime);
+        return move;
+    }
+
+}

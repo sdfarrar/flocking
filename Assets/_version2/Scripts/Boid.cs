cf22c98c3921aaa69413230ad558345fc26c6bc2
sdfarrar <vague315@gmail.com>
2019-09-11 13:41:00 -0500

    Adds basic boid. Adds generator

---
+++ Assets/_version2/Scripts/Boid.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour {
+
+    [SerializeField] private new SpriteRenderer renderer;
+
+
+#if UNITY_EDITOR
+    void OnDrawGizmos() {
+        if(renderer==null){ return; }
+        GameObject[] selected = UnityEditor.Selection.gameObjects;
+        if(UnityEditor.Selection.activeGameObject != this.gameObject){
+            renderer.color = Color.white;
+            return;
+        }
+        renderer.color = Color.red;
+    }
+#endif
+}
---
+++ Assets/_version2/Scripts/BoidsGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidsGenerator : MonoBehaviour {
+
+    [SerializeField] private Boid prefab;
+    private List<Boid> boids = new List<Boid>();
+
+    [Range(0, 255)]
+    [SerializeField] private int total = 50;
+    [SerializeField] private float density = 0.08f;
+    [SerializeField] private float radius = 5f;
+
+
+    void Start() {
+        for (int i = 0; i < total; ++i) {
+            Vector3 position = Random.insideUnitCircle * total * density * radius;
+            Quaternion rotation = Quaternion.Euler(Vector3.forward * Random.Range(0f, 360f));
+            Boid boid = Instantiate(prefab, position, rotation, transform);
+            boid.name = "Boid " + i;
+            boids.Add(boid);
+        }
+    }
+}

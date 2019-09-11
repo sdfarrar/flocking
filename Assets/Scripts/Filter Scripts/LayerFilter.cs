27d993b8fc03e73ad3fc69875d0b6914fb112691
sdfarrar <vague315@gmail.com>
2019-09-11 13:01:40 -0500

    Adds filter to avoid objects on specific layers

---
+++ Assets/Scripts/Filter Scripts/LayerFilter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Filter/Layer")]
+public class LayerFilter : ContextFilter {
+
+    [SerializeField] private LayerMask mask;
+
+    public override List<Transform> Filter(FlockAgent agent, List<Transform> list) {
+        return list.FindAll( transform => {
+            bool isLayerInMask = (mask == (mask | (1 << transform.gameObject.layer)));
+            return isLayerInMask;
+        });
+    }
+
+}
